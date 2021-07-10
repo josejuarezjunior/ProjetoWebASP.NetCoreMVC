6b5608bfa51b8e250faa7cc38cc3f237058c015c
Jose Juarez <juarez.junior@outlook.com>
2021-07-10 11:36:03 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        //Excessão personalizada
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
